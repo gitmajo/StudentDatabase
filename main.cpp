7b088fd691f304df9f8122f281b3005597b602b4
PrzemekB <przemb@yahoo.com>
2019-01-19 23:13:56 +0100

    SortStudents added, printStudents added.

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <iomanip>
+#include <algorithm>
 
 class Student
 {
@@ -15,7 +16,10 @@ class Student
             : _name(name), _surname(surname), _index(index) {}
 
         friend std::ostream& operator<<(std::ostream& os, const Student& student);
+        friend bool compare(const Student& leftSide, const Student& rightSide);
 };
+
+bool compare(const Student& leftSide, const Student& rightSide);
 
 class Database
 {
@@ -25,13 +29,33 @@ class Database
     public:
         void addStudent(const std::string name, const std::string surname, const int index);
         void delStudent(const std::string name, const std::string surname, const int index);
-     
+        void sortStudents();
+        void printStudents();
 };
+
+
 
 void Database::addStudent(std::string name, std::string surname, int index)
 {
     Student student(name, surname, index);
     data.push_back(student);
+}
+
+void Database::sortStudents()
+{
+    std::sort(data.begin(), data.end(), compare);
+}
+
+void Database::printStudents()
+{
+    for(const auto& student : data)
+        std::cout << student;
+    std::cout << "\n";
+}
+
+bool compare(const Student& leftSide, const Student& rightSide)
+{
+    return leftSide._index < rightSide._index;
 }
 
 
@@ -48,15 +72,16 @@ std::ostream& operator<<(std::ostream& os, const Student& student)
 int main(){
     std::cout << "\nProject1: StudentDatabase\n\n";
     
-    Database studentBase; 
+    Database base
 
-    studentBase.addStudent("Adam",  "Malinowski",  12345);
-    studentBase.addStudent("Tomek", "Kowalski",    55555);
-    studentBase.addStudent("Kamil", "Dabrowski",   33333);
-    studentBase.addStudent("Ada",   "Kwiatkowska", 22222);
+    base.addStudent("Adam",  "Malinowski",  12345);
+    base.addStudent("Tomek", "Kowalski",    55555);
+    base.addStudent("Kamil", "Dabrowski",   33333);
+    base.addStudent("Ada",   "Kwiatkowska", 22222);
 
-
-
+    base.printStudents();
+    base.sortStudents();
+    base.printStudents();
 
 return 0;
 }
