8aec5accfd18078b07c3fb2c3c75861d724d6f92
PrzemekB <przemb@yahoo.com>
2019-01-19 16:39:37 +0100

    Student constructor added

--- main.cpp
+++ main.cpp
@@ -3,14 +3,25 @@
 
 class Student
 {
-	std::string name;
-	std::string surname;
-	int index;
+    private:
+        std::string _name {""};
+        std::string _surname {""};
+        int _index {0};
+    
+    public:
+        Student(std::string name, std::string surname, int index)
+            : _name(name), _surname(surname), _index(index) {}
 };
 
 
 int main(){
-	std::cout << "\nProject1: StudentDatabase\n\n";
+    std::cout << "\nProject1: StudentDatabase\n\n";
+    
+    Student Adam("Adam", "Malinowski",  12345);
+    Student Tomek("Tomek", "Kowalski",  55555);
+    Student Kamil("Kamil", "Dabrowski", 33333);
+    Student Ada("Ada", "Kwiatkowska",   22222);
+
 
 return 0;
 }
