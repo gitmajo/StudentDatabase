91b40a0279b10d8beb462d7e963da95e06be127f
Damian Stasiak <stasiakdamian89@gmail.com>
2019-01-19 12:28:52 +0100

    class Student v1

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+class Student
+{
+std::string imie;
+std::string nazwisko;
+int indeks;
+};
+
+
+int main(){
+	std::cout << "\nProject1: StudentDatabase\n\n";
+
+return 0;
+}
