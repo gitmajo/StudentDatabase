39f76c69923af02dc1a57aa369aa25a164a13325
PrzemekB <przemb@yahoo.com>
2019-01-19 18:57:33 +0100

    addStudent method added

--- main.cpp
+++ main.cpp
@@ -23,10 +23,18 @@ class Database
         std::vector <Student> data;
 
     public:
-        void addstudent(std::string name, std::string surname, int index);
-        void delstudent(std::string name, std::string surname, int index);
+        void addStudent(const std::string name, const std::string surname, const int index);
+        void delStudent(const std::string name, const std::string surname, const int index);
      
 };
+
+void Database::addStudent(std::string name, std::string surname, int index)
+{
+    Student student(name, surname, index);
+    data.push_back(student);
+}
+
+
 std::ostream& operator<<(std::ostream& os, const Student& student)
 {
     os << std::left << std::setw(13) << student._surname << " "
@@ -37,16 +45,18 @@ std::ostream& operator<<(std::ostream& os, const Student& student)
 
 
 
-
 int main(){
     std::cout << "\nProject1: StudentDatabase\n\n";
     
-    Student Adam("Adam", "Malinowski",  12345);
-    Student Tomek("Tomek", "Kowalski",  55555);
-    Student Kamil("Kamil", "Dabrowski", 33333);
-    Student Ada("Ada", "Kwiatkowska",   22222);
+    Database studentBase; 
 
-    std::cout << Adam << Tomek << Kamil << Ada;
+    studentBase.addStudent("Adam",  "Malinowski",  12345);
+    studentBase.addStudent("Tomek", "Kowalski",    55555);
+    studentBase.addStudent("Kamil", "Dabrowski",   33333);
+    studentBase.addStudent("Ada",   "Kwiatkowska", 22222);
+
+
+
 
 return 0;
 }
